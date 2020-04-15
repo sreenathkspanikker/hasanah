02032b49ff9cd50b770fe3a13cb4cf98951496f8
Sreenath Ks <sreenathkspanikker@users.noreply.github.com>
2020-04-15 14:40:31 +0400

    Updates

---
+++ src/modules/donation.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import DonationList from "../componentes/donationList";
+import Addmore from "../componentes/addmore";
+import Total from "../componentes/total";
+import { Row, Col, Button } from "react-bootstrap";
+
+const Donation = () => {
+  const [state, setState] = useState(0);
+
+  return (
+    <div className="app-donation">
+      <h2>Donation list</h2>
+      <Row>
+        <Col lg={7}>
+          <DonationList totlAmount={(amount) => setState(amount)} />
+        </Col>
+        <Col lg={5}>
+          <div className="wrap">
+            <Addmore />
+            <Total amount={state} />
+          </div>
+          <article className="article-donation">
+            <p>
+              You have chosen a monthly recurring donation, Your creadit card
+              will be changed $125.00 today. Starting next month, your credit
+              card will be charged $15.00 each month..
+            </p>
+            <Button>Learn more</Button>
+          </article>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Donation;
---
+++ src/modules/project.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  ButtonGroup,
+  Button,
+  Card,
+  Row,
+  Col,
+  ProgressBar,
+} from "react-bootstrap";
+import Api from "../utils/project.json";
+import Icon from "../componentes/icon";
+
+const Project = () => {
+  return (
+    <div className="projects">
+      <h2>
+        You may also like these projects
+        <Button>Descover all</Button>
+      </h2>
+
+      <Row>
+        {Api.projects.map((d, i) => {
+          return (
+            <Col md={6} lg={4}>
+              <Card className="card-project">
+                <Card.Img variant="top" src={d.img} />
+                <Card.Body>
+                  <ul>
+                    <li>Animals</li>
+                    <li>Egypt</li>
+                  </ul>
+                  <Card.Title>{d.title}</Card.Title>
+                  <Card.Text>
+                    <strong>{d.price}</strong> {d.raised}
+                  </Card.Text>
+                  <ProgressBar now={d.progress} />
+                  <Card.Text>{d.date}</Card.Text>
+                  <ButtonGroup aria-label="Basic example">
+                    <Button variant="primary">Donate</Button>
+                    <Button variant="like">
+                      <Icon name="like" size="1.3em" />
+                    </Button>
+                  </ButtonGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
+
+export default Project;
