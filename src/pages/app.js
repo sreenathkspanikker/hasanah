0520ac11d776c1ef39f7a3603e894dab00a07c72
Sreenath Ks <sreenathkspanikker@users.noreply.github.com>
2020-04-15 12:32:08 +0400

    Page loader
    Added Page loader

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,22 +3,34 @@ import Layout from "../layout/layout";
 import Message from "../componentes/message";
 import Donation from "../modules/donation";
 import Project from "../modules/project";
+import Pageloader from "../componentes/pageloader";
 
 const App = () => {
   const [state, setstate] = useState(false);
+  const [isLoader, setLoader] = useState(false);
 
   setInterval(() => !state && setstate(true), 5000);
 
   useEffect(() => {
     state && setTimeout(() => setstate(false), 5000);
+
+    setTimeout(() => {
+      setLoader(true);
+    }, 2000);
   }, [state]);
 
   return (
-    <Layout>
-      <Message className={`ml-auto ${state ? "bounceIn" : "bounceOut"}`} />
-      <Donation />
-      <Project />
-    </Layout>
+    <React.Fragment>
+      {isLoader ? (
+        <Layout>
+          <Message className={`ml-auto ${state ? "bounceIn" : "bounceOut"}`} />
+          <Donation />
+          <Project />
+        </Layout>
+      ) : (
+        <Pageloader />
+      )}
+    </React.Fragment>
   );
 };
 
